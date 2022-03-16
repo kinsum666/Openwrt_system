696a3598ee7e8f9832ccdc035b6478a1c221f8ce
kinsum <79512621+kinsum666@users.noreply.github.com>
2022-03-16 11:12:49 +0800

    Create 4300diy1.sh

---
+++ 4300diy1.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+#
+
+# Uncomment a feed source
+# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
+
+sed -i "/helloworld/d" "feeds.conf.default"
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+
+# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
+# echo 'src-git OpenAppFilter https://github.com/destan19/OpenAppFilter' >>feeds.conf.default
+# echo 'src-git luci-app-easymesh https://github.com/kinsum666/luci-app-easymesh.git' >>feeds.conf.default
