b5e19845d5ca1b7d208950221b813631d6f5f841
kinsum <79512621+kinsum666@users.noreply.github.com>
2022-03-16 11:17:01 +0800

    Update 4300diy1.sh

--- 4300diy1.sh
+++ 4300diy1.sh
@@ -9,9 +9,7 @@
 #
 
 # Uncomment a feed source
-# sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
-sed -i "/helloworld/d" "feeds.conf.default"
+sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
